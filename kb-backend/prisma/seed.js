e3db41cf8ea21bd0f5a8ac2f2d56dd4acba4445e
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-17 15:16:58 -0700

    Add db setup

---
+++ kb-backend/prisma/seed.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+    /* const boards = [
+        { name: 'Team Celebration', category: 'Celebration', author: "Mike" }
+    ]
+
+    const cards = [
+        { title: "Great job on the release!", description: "You crushed it.", gif: "https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif", author: "Alice", boardId: 1 }
+    ]
+
+    for (const board of boards) {
+        await prisma.board.create({ data: board });
+    }
+
+    for (const card of cards) {
+        await prisma.card.create({ data: card });
+    }
+
+    console.log("Seeded boards with cards!"); */
+}
+
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
