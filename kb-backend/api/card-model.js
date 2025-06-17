e54b65c6ab6237d3261f14475b8d6cb168b0cd4c
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-17 15:40:04 -0700

    Implement card modal

---
+++ kb-backend/api/card-model.js
@@ -0,0 +1,40 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+module.exports = {
+    async find(where) {
+        // GET http://localhost:5432/api/card?type=dog
+        // SELECT * FROM "Card" WHERE type='dog';
+        const cards = await prisma.card.findMany({ where })
+        return cards
+    },
+
+    async findById(id) {
+        // GET http://localhost:5432/api/card/1
+        // SELECT * FROM "Card" WHERE id = 1;
+        const card = await prisma.card.findUnique({ where: { id } })
+        return card
+    },
+
+    async create(changes) {
+        // POST http://localhost:5432/api/card/1 { name: "Fido", type: "dog": age: 5 }
+        // INSERT INTO "Card" (name, type, age) VALUES ('Fido', 'dog', 5);
+        const card = await prisma.card.create({ data: changes })
+        return card
+    },
+
+    async update(id, changes) {
+        // PUT http://localhost:5432/api/card/1 { adopted: true }
+        // UPDATE "Card" SET adopted = true WHERE id = 1;
+        const card = await prisma.card.update({ where: { id }, data: changes })
+        return card
+    },
+
+    async delete(id) {
+        // DELETE http://localhost:5432/api/card/1
+        // DELETE FROM "Card" WHERE id = 1;
+        const card = await prisma.card.delete({ where: { id } })
+        return card
+    }
+}
