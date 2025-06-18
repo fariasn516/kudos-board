d71719e5ffb3917cb1989f826571f621a9f05072
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-18 11:35:18 -0700

    Add css styling

--- kb-frontend/kudos-board-fe/src/components/Footer.jsx
+++ kb-frontend/kudos-board-fe/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-          <p>&copy; 2025 Kudos Board by Nancy F.</p>
+    <footer>
+          <p className='footer-content'>&copy; 2025 Kudos Board by Nancy F.</p>
     </footer>
   );
 };
